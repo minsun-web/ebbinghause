78bfc61ccef7e71c3661711136330f10aac496ca
minsun-web <whalstjs7@naver.com>
2025-05-08 18:33:09 +0900

    Update ebbinghaus_scheduler.py

--- ebbinghaus_scheduler.py
+++ ebbinghaus_scheduler.py
@@ -108,9 +108,26 @@ if current_user:
         st.dataframe(df)
 
         # 복습 완료 표시
-        st.header('Edit or Update Entries')
         if len(df) > 0:
-    st.header('Edit or Update Entries')
+                    # 수정할 항목 선택
+            edit_content = st.selectbox('Select an entry to edit:', df['Content'])
+            new_content = st.text_input('Update Content', value=edit_content)
+            update_status = st.checkbox('Mark as Completed', value=bool(df.loc[df['Content'] == edit_content, 'Completed'].values[0]))
+            if st.button('Update Entry'):
+                df.loc[df['Content'] == edit_content, 'Content'] = new_content
+                df.loc[df['Content'] == new_content, 'Completed'] = update_status
+                df.to_csv(data_path, index=False)
+                st.success(f'Updated "{new_content}"!')
+        if len(df) > 0:
+            # 수정할 항목 선택
+            edit_content = st.selectbox('Select an entry to edit:', df['Content'])
+            new_content = st.text_input('Update Content', value=edit_content)
+            update_status = st.checkbox('Mark as Completed', value=bool(df.loc[df['Content'] == edit_content, 'Completed'].values[0]))
+            if st.button('Update Entry'):
+                df.loc[df['Content'] == edit_content, 'Content'] = new_content
+                df.loc[df['Content'] == new_content, 'Completed'] = update_status
+                df.to_csv(data_path, index=False)
+                st.success(f'Updated "{new_content}"!')
     # 수정할 항목 선택
     edit_content = st.selectbox('Select an entry to edit:', df['Content'])
     new_content = st.text_input('Update Content', value=edit_content)
